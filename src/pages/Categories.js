d6dd77abcc7a5335d9342eab06146c0dc16532bf
iamsid <developer.sid@outlook.com>
2022-08-16 13:19:33 +0300

    subscribe categories status button to the redux store

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
 
-const Categories = () => (
-  <div>
-    <button type="button">
-      Check Status
-    </button>
-  </div>
-);
+const Categories = () => {
+  const dispatch = useDispatch();
+  const status = useSelector((state) => state.categories);
+
+  const checkStatusHandler = () => {
+    dispatch(checkStatus('Under construction'));
+  };
+
+  return (
+    <div>
+      <button type="button" onClick={checkStatusHandler}>
+        Check Status
+      </button>
+      {status && <h2>{status}</h2>}
+    </div>
+  );
+};
 
 export default Categories;
