bacb96839fe07a538533543dd838d3c3e196a4e7
iamsid <developer.sid@outlook.com>
2022-08-16 13:05:18 +0300

    subscribe addBook component to the redux store, add remove book functionality

--- src/redux/books/book.js
+++ src/redux/books/book.js
@@ -1,7 +1,20 @@
 const ADD = 'bookstore/books/ADD';
 const REMOVE = 'bookstore/books/REMOVE';
 
-export default function booksReducer(state = [], action) {
+const initialState = [
+  {
+    id: '1',
+    title: 'Book Lovers',
+    author: 'Emily Henry',
+  },
+  {
+    id: '2',
+    title: 'Finding Me',
+    author: 'Viola Davis',
+  },
+];
+
+export default function booksReducer(state = initialState, action) {
   switch (action.type) {
     case ADD:
       return [...state, action.book];
