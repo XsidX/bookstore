6c5fe518b235751f50390c48b2aa3a2513d4ba0e
iamsid <developer.sid@outlook.com>
2022-08-18 09:44:45 +0300

    refactor addBook, removeBook to make api calls using createAsyncThunk

--- src/components/UI/Book.js
+++ src/components/UI/Book.js
@@ -2,15 +2,17 @@ import React from 'react';
 import propTypes from 'prop-types';
 
 import { useDispatch } from 'react-redux';
-import { removeBook } from '../../redux/books/book';
+// import { bookActions } from '../../redux/books/books-slice';
+import { deleteBook } from '../../redux/books/book';
 
 const Book = ({ book }) => {
   const dispatch = useDispatch();
-  const { id, title, author } = book;
+  const { item_id: id, title, author } = book;
 
   const removeBookHandler = (e) => {
     e.preventDefault();
-    dispatch(removeBook(id));
+
+    dispatch(deleteBook(id));
   };
   return (
     <>
@@ -27,7 +29,7 @@ const Book = ({ book }) => {
 
 Book.propTypes = {
   book: propTypes.shape({
-    id: propTypes.string.isRequired,
+    item_id: propTypes.string.isRequired,
     title: propTypes.string.isRequired,
     author: propTypes.string.isRequired,
   }).isRequired,
